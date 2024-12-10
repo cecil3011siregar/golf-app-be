8fd94abe531ca3f07e81b70c5e5f6c708034db6d
farellandr <farellandreano@gmail.com>
2024-12-10 14:30:17 +0700

    feat: holiday entity

--- src/holiday/entities/holiday.entity.ts
+++ src/holiday/entities/holiday.entity.ts
@@ -1,1 +1,37 @@
-export class Holiday {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Holiday {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+  
+  @Column({ type: 'int' })
+  price: number;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'varchar', length: 60 })
+  duration: string;
+
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+    nullable: false,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
--- src/holiday/holiday.module.ts
+++ src/holiday/holiday.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HolidayService } from './holiday.service';
 import { HolidayController } from './holiday.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Holiday } from './entities/holiday.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Holiday]),
+  ],
   controllers: [HolidayController],
   providers: [HolidayService],
 })
