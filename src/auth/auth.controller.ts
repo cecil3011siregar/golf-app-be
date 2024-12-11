3629eacfb8ee2920ed95134b4c4a1de72e158ee7
farellandr <farellandreano@gmail.com>
2024-12-11 15:44:47 +0700

    fix: public login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { Public } from './decorators/public.decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('/login')
   async create(@Body() loginDto: LoginDto) {
     return {
