b7e2a65b0697b5499fdad3ff9fd4f4ded1f2c110
moonrill <arilramadani245@gmail.com>
2024-12-12 15:00:20 +0700

    feat: add sport type image

--- src/image/entities/image.entity.ts
+++ src/image/entities/image.entity.ts
@@ -1,11 +1,14 @@
 import { Holiday } from '#/holiday/entities/holiday.entity';
+import { SportType } from '#/sport-type/entities/sport-type.entity';
 import { Sport } from '#/sport/entities/sport.entity';
 import {
   Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -48,4 +51,8 @@ export class Image {
   holiday: Holiday;
   @Column({ type: 'uuid', nullable: true })
   holidayId: string;
+
+  @OneToOne(() => SportType, (sportType) => sportType.image, { nullable: true })
+  @JoinColumn({ name: 'sport_type_id' })
+  sportType: SportType;
 }
